4f282a9b54c9afcf589d7e0851fd9c1118484300
Tashrin Tanisha Ishq <145369121+TashrinTanisha@users.noreply.github.com>
2024-03-22 01:14:27 +0600

    Print

---
+++ String-7.c
@@ -0,0 +1,17 @@
+/* Write a C program to print the following output :
+
+   Enter your name :
+   Tashrin Tarannum Tanisha Ishq //this will be given by user using keyword
+   Hello! Tashrin Tarannum Tanisha Ishq */
+#include<stdio.h>
+int main()
+{
+    char name[50] ;
+
+    printf("Enter your name : ") ;
+    gets(name) ;
+
+    printf("Hello! %s\n", name);
+
+    return 0;
+}
