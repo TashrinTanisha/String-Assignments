e32386594b0c75c01ff522d25c8fc1f7662ecd88
Tashrin Tanisha Ishq <145369121+TashrinTanisha@users.noreply.github.com>
2024-03-25 01:10:13 +0600

    Finished

---
+++ String-13.c
@@ -0,0 +1,24 @@
+/* Write a C program which will take a word as input from the user and print
+   the length of the string without using any built in function */
+#include<stdio.h>
+int main()
+{
+    char name[50] ;
+
+    printf("Enter your name : ");
+    scanf("%s", name) ;
+
+    int ind = 0, counter = 0 ;
+
+    while(name[ind] != '\0')
+    {
+
+        counter++ ;
+        ind++ ;
+
+    }
+
+    printf("The length of the string : %d\n", counter);
+
+    return 0 ;
+}
