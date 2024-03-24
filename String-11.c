f3a9ea424c141c128adc711de526e81a3481e54a
Tashrin Tanisha Ishq <145369121+TashrinTanisha@users.noreply.github.com>
2024-03-25 01:09:23 +0600

    Completed

---
+++ String-11.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    char str[] = "Ishq" ;
+
+    int ind = 0 ;
+
+    while(1)
+    {
+
+        printf("%d = %c\n", ind, str[ind]);
+        if(str[ind] == '\0')
+        {
+
+            break;
+        }
+        ind++ ;
+    }
+
+    return 0;
+}
